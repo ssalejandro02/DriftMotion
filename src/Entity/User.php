b606e35a5fcbde8610a9619d10bf83f4d321badb
ssalejandro02 <ssalejandro02@gmail.com>
2024-02-22 17:00:15 +0100

    Corrijo entidad de user para que el nombre de usuario sea único

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,7 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $username = null;
 
     #[ORM\Column]
